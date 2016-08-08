e1d122ca0c32895798f33c6943d7770e5b53d966
thereqwe@qq.com <3454453q>
2016-08-08 18:19:53 +0800

    new home

---
+++ SoundLive/Viewcontroller/YKSHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  YKSHomeViewController.h
+//  SoundLive
+//
+//  Created by Yue Shen on 16/8/8.
+//  Copyright © 2016年 Yue Shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YKSHomeViewController : UIViewController
+
+@end
