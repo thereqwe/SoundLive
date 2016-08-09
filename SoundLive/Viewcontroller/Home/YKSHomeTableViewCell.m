1add4285d2f02c0f5e9ede55db788f3afe94006b
thereqwe@qq.com <3454453q>
2016-08-09 14:42:07 +0800

    123

--- SoundLive/Viewcontroller/Home/YKSHomeTableViewCell.m
+++ SoundLive/Viewcontroller/Home/YKSHomeTableViewCell.m
@@ -39,5 +39,32 @@
         make.top.right.left.mas_equalTo(0);
         make.height.mas_equalTo(80);
     }];
+    
+    ui_lb_title = [UILabel new];
+    [ui_view_container addSubview:ui_lb_title];
+    [ui_lb_title mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(8);
+        make.right.mas_equalTo(-8);
+        make.top.equalTo(ui_img_poster.mas_bottom).offset(8);
+    }];
+    
+    ui_lb_begin_time = [UILabel new];
+    [ui_view_container addSubview:ui_lb_begin_time];
+    [ui_lb_begin_time mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(8);
+        make.top.equalTo(ui_lb_title.mas_bottom).offset(8);
+    }];
+    
+    ui_img_show_status = [UIImageView new];
+    [ui_view_container addSubview:ui_img_show_status];
+    [ui_img_show_status mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(ui_lb_title.mas_bottom).offset(8);
+        make.right.mas_equalTo(-8);
+    }];
+}
+
+- (void)setDataWithDict:(NSDictionary*)dict
+{
+    [ui_img_show_status sd_setImageWithURL:nil];
 }
 @end
