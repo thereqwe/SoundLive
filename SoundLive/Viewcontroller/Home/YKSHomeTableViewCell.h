2b986a6918366ff532f6eac43f6c7e4aec4dc4eb
thereqwe@qq.com <3454453q>
2016-08-09 10:53:02 +0800

    123

---
+++ SoundLive/Viewcontroller/Home/YKSHomeTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  YKSHomeTableViewCell.h
+//  SoundLive
+//
+//  Created by Yue Shen on 16/8/9.
+//  Copyright © 2016年 Yue Shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YKSHomeTableViewCell : UITableViewCell
+
+@end
---
+++ SoundLive/Viewcontroller/Home/YKSHomeTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  YKSHomeTableViewCell.m
+//  SoundLive
+//
+//  Created by Yue Shen on 16/8/9.
+//  Copyright © 2016年 Yue Shen. All rights reserved.
+//
+
+#import "YKSHomeTableViewCell.h"
+
+@implementation YKSHomeTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
